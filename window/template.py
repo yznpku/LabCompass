f1884d2f51f8cc12cbb84e9b1aa4f61f19a9f3b8
FutureCode <yzn20080@gmail.com>
2017-10-12 19:05:06 +1100

    Make all windows take no focus.

--- window/template.py
+++ window/template.py
@@ -1,3 +1,7 @@
+import ctypes
+import win32con
+import win32gui
+
 from PyQt5.QtCore import Qt, QUrl, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtQuickWidgets import QQuickWidget
@@ -11,9 +15,9 @@ class Window(QQuickWidget):
     self.setAttribute(Qt.WA_TranslucentBackground)
     self.setAttribute(Qt.WA_TransparentForMouseEvents, 'transparent' in kwargs and kwargs['transparent'])
     if 'frameless' in kwargs and kwargs['frameless']:
-      self.setWindowFlags(Qt.Tool | Qt.WindowDoesNotAcceptFocus | Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+      self.setWindowFlags(Qt.Tool | Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
     else:
-      self.setWindowFlags(Qt.Tool | Qt.WindowDoesNotAcceptFocus | Qt.WindowStaysOnTopHint)
+      self.setWindowFlags(Qt.Tool | Qt.WindowStaysOnTopHint)
     self.setSource(QUrl(qmlPath))
 
     if 'parent' in kwargs:
@@ -30,6 +34,11 @@ class Window(QQuickWidget):
   @pyqtSlot()
   def repositionToParent(self):
     self.move(self.parentWindow.quickWindow().x() + self.offset[0], self.parentWindow.quickWindow().y() + self.offset[1])
+
+  def showEvent(self, event):
+    super().showEvent(event)
+    hwnd = int(self.winId())
+    ctypes.windll.user32.SetWindowLongW(hwnd, win32con.GWL_EXSTYLE, ctypes.windll.user32.GetWindowLongW(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_APPWINDOW)
 
 class TransparentWindow(Window):
   def __init__(self, engine, qmlPath, **kwargs):
