0575f6040ade8f9bcea16f4d1f45595759f20900
FutureCode <yzn20080@gmail.com>
2018-01-24 01:31:37 +0800

    Fix client path configuration unable to select Steam client.

--- common/src/window/optionswindow.cpp
+++ common/src/window/optionswindow.cpp
@@ -30,7 +30,7 @@ void OptionsWindow::onWindowOpenChanged()
 
 void OptionsWindow::onBrowseClientPath()
 {
-  auto file = QFileDialog::getOpenFileName(this, "Find Game Client", "", "Path of Exile Client (Client.exe)");
+  auto file = QFileDialog::getOpenFileName(this, "Find Game Client", "", "Path of Exile Client (Client.exe;PathOfExileSteam.exe)");
   if (!file.isEmpty())
      clientPathInput->setProperty("text", QFileInfo(file).dir().absolutePath());
 }
