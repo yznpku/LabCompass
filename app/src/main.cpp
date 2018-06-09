48f431f711c2916c0c8342ad3254d5fcb74ca934
FutureCode <yzn20080@gmail.com>
2018-06-09 12:55:01 +1000

    Set qml material style.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char** argv)
   qputenv("QT_SCALE_FACTOR", scaleFactor);
 
   qputenv("QML_DISABLE_DISK_CACHE", "1");
+  qputenv("QT_QUICK_CONTROLS_MATERIAL_THEME", "Dark");
+  qputenv("QT_QUICK_CONTROLS_MATERIAL_ACCENT", "#2196F3");
   Application app(argc, argv);
   return app.exec();
 }
