11e6f0bd7be73795ad110446698a73863b388c0e
FutureCode <yzn20080@gmail.com>
2018-07-13 05:31:45 +1000

    1.3.4 release.

--- common/src/global.h
+++ common/src/global.h
@@ -3,7 +3,7 @@
 
 #include "stdafx.h"
 
-static const QString VERSION = "1.3.3";
+static const QString VERSION = "1.3.4";
 
 using RoomId = QString;
 using DirectionCode = QString; // [""(Unknown), "C"(Secret Passage), "N", "NE", "E", "SE", "S", "SW", "W", "NW"]
